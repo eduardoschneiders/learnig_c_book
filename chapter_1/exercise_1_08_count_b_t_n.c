03017e628decc441bb50c02781ec0389d481aafd
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2023-07-15 13:42:58 -0300

    1_08 count chars

--- chapter_1/exercise_1_08_count_b_t_n.c
+++ chapter_1/exercise_1_08_count_b_t_n.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
 int main(){
-  int c;
+  int c, b, t, n;
+  b = t = n = 0;
 
   while ((c = getchar()) != EOF) {
-    printf("%d", EOF);
-    putchar('-');
-    putchar(c);
+    if (c == ' ')
+      b++;
+    else if (c == '\t')
+      t++;
+    else if (c == '\n')
+      n++;
   }
+
+  printf("\nb: %d, t: %d, n: %d\n", b, t, n);
 
   return 0;
 }
